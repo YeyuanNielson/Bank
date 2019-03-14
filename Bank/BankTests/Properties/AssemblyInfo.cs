487374eb25a711a53d8f29ecd637fa93d8bdfc62
yeyuans <ysong@conservice.com>
2019-03-14 14:33:52 -0600

    commit initial change for the unit test

---
+++ Bank/BankTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BankTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Service")]
+[assembly: AssemblyProduct("BankTests")]
+[assembly: AssemblyCopyright("Copyright © Service 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e60164e6-5644-4b96-8a38-957600b6c795")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
